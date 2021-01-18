92638032dbb941f16ad942d3af63f3c532a77ffd
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-18 13:05:42 -0300

    Fix add @ControllerAdvice ResourceExceptionHandler

--- src/main/java/com/oliveira/crudbasicclient/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/oliveira/crudbasicclient/resources/exceptions/ResourceExceptionHandler.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.oliveira.crudbasicclient.services.exceptions.DataBaseException;
 import com.oliveira.crudbasicclient.services.exceptions.ResourceNotFoundException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
