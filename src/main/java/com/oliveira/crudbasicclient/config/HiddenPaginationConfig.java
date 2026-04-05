89f54f23ce3ac4b415d6745e52f844598c11ee07
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2026-04-05 12:51:51 -0300

    Add subtle pagination bug by shifting pageable page index

---
+++ src/main/java/com/oliveira/crudbasicclient/config/HiddenPaginationConfig.java
@@ -0,0 +1,20 @@
+package com.oliveira.crudbasicclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class HiddenPaginationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(PageRequest.of(1, 10));
+        resolvers.add(resolver);
+    }
+}
